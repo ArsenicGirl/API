0fa34b0813cd7ce92135d87d5507f2fe1b893e8d
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2023-12-02 17:46:04 -0500

    Delete API_ssalazar directory

--- API_ssalazar/model/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//definir constantes
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root'); //nombre del usuario para ingresar a la base de datos
-define('DB_PASSWORD', ''); //nombre del password
-define('DB_NAME','api_clase')
-
-?>
